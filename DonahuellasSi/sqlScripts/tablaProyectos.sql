945da868a90638842ba175c46a6b64d9c7a166b6
Bárbara <barbaradd33@gmail.com>
2026-05-07 16:06:41 -0400

    tabla aporte y proyecto listas

---
+++ DonahuellasSi/sqlScripts/tablaProyectos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[proyecto]
+(
+	[id_proyecto] INT NOT NULL PRIMARY KEY identity,
+	[nombre_proyecto] VARCHAR(100) NOT NULL,
+	[descripcion_proyecto] VARCHAR(255) NOT NULL,
+	[monto_donado] INT NOT NULL
+)
+GO
